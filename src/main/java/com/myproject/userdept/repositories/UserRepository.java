a5728e6ad74b28d1e86230f9660f0f3dc696c595
jheiniffer oliveira <jheiniffer2013@gmail.com>
2023-06-25 20:17:35 -0300

    Project Spring Boot

---
+++ src/main/java/com/myproject/userdept/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.myproject.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myproject.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+
+}
